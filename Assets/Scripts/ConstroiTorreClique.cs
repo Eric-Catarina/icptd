a521813ae0292d64bab402a4ef150551e290baac
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-07 18:37:12 -0300

    Adicao Sistema Clique --> Gera Torre

--- Assets/Scripts/ConstroiTorreClique.cs
+++ Assets/Scripts/ConstroiTorreClique.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ConstroiTorreClique : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject torrePrefab;
+    private bool Clicou()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
     void Start()
     {
-        
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        if (Clicou())
+        {
+            Debug.Log("clicou");
+            
+            Vector3 pontoDoClique = Input.mousePosition;
+            Ray raioDaCamera = Camera.main.ScreenPointToRay (pontoDoClique);
+            
+            float comprimentoMaximo = 10000000.0f;
+            
+            Physics.Raycast (raioDaCamera, out RaycastHit infoDoRaio, comprimentoMaximo);
+            
+            if (infoDoRaio.collider)
+            {
+                Vector3 posicaoDoElemento = infoDoRaio.point;
+                Instantiate(torrePrefab, posicaoDoElemento, Quaternion.identity);
+            }
+        }
     }
 }
